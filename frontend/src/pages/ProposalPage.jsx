7bd6efbd6f08276a6fd1b4c8bb842ed9aebfee3e
rosebellotaza <rosebellnarieotaza@gmail.com>
2026-03-05 15:03:27 +0800

    add gitignore

--- frontend/src/pages/ProposalPage.jsx
+++
@@ -1,9 +0,0 @@
-import ProposalForm from '../components/ProposalForm'
-
-export default function ProposalPage() {
-  return (
-    <div style={{ maxWidth: '800px', margin: '40px auto', padding: '0 20px' }}>
-      <ProposalForm />
-    </div>
-  )
-}
